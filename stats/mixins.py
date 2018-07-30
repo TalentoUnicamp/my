d34b0be989174c801ddea93eeb076262b06c35e1
Gustavo Maronato <gustavomaronato@gmail.com>
2018-07-29 21:13:22 -0300

    implemented some new stats. addresses #10 (#32)

--- stats/mixins.py
+++ stats/mixins.py
@@ -14,7 +14,9 @@ class StatsContextMixin(ContextMixin):
         context = super().get_context_data(**kwargs)
         stats_context = {
             'api': {
-                'hacker_stats': reverse('stats:api:hacker_stats')
+                'hacker_stats': reverse('stats:api:hacker_stats'),
+                'hacker_signup_list': reverse('stats:api:hacker_signup_list'),
+                'hacker_application_list': reverse('stats:api:hacker_application_list'),
             }
         }
         context['stats_context'] = json.dumps(stats_context)
--- stats/urls.py
+++ stats/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from . import views, api
 
 apipatterns = [
-    path('hacker_stats/', api.HackerStats.as_view(), name='hacker_stats')
+    path('hacker_stats/', api.HackerStats.as_view(), name='hacker_stats'),
+    path('hacker_signup_list/', api.HackerSignupList.as_view(), name='hacker_signup_list'),
+    path('hacker_application_list/', api.HackerApplicationList.as_view(), name='hacker_application_list'),
 ]
 
 app_name = 'stats'
 urlpatterns = [
     path('api/', include((apipatterns, 'api')), name='api'),
-    path('', views.StatsView.as_view(), name='index'),
+    path('', views.StatsView.as_view(), name='index')
 ]
